9b897a41a5a21bd8eb33b1472fa8c5ed8b660250
Tommy Scribble <tommy@scribbledesign.co.uk>
2018-08-27 13:05:58 +0100

    add moving ball tot he canvas

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 var canvas = document.getElementById('myCanvas'),
-    ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d'),
+    x = canvas.width / 2,
+    y = canvas.height - 30,
+    dx = 2,
+    dy = -2;
 
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+setInterval(draw, 10);
