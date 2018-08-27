2f1eb11be57219ba3fd8753c20e7f05e6b154c95
Tommy Scribble <tommy@scribbledesign.co.uk>
2018-08-27 09:51:26 +0100

    add postcss settings to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
-var gulp = require('gulp'),
-    postcss = require('gulp-postcss'),
-    shortColor = require('postcss-short-color');
-
-gulp.task('css', function() {
-    return gulp.src('initial/*.css')
-    .pipe(postcss([shortColor]))
+const gulp = require('gulp');
+gulp.task('css', () => {
+const postcss = require('gulp-postcss'),
+    sourceMaps = require('gulp-sourcemaps'),
+    cssImport = require('postcss-import'),
+    precss = require('precss'),
+    autoprefixer = require('autoprefixer'),
+    minify = require('cssnano');
+    return gulp
+    .src('initial/*.css')
+    .pipe(sourceMaps.init())
+    .pipe(postcss([
+        cssImport(),
+        precss(),
+        autoprefixer(),
+        minify(),
+    ]))
+    .pipe(sourceMaps.write('.'))
     .pipe(gulp.dest('final'));
-})
+});
